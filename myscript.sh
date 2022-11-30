b49cf2952fe70b90f35ba619d4229cfbd58e22ed
Marta_lap <megabuu@gmail.com>
2022-11-30 22:05:21 +0100

    koniec psot

--- myscript.sh
+++ myscript.sh
@@ -9,3 +9,27 @@ else
  echo "niebardzo" 
 
 fi
+
+if [ -f ./test.txt  ]
+ then 
+    ls
+    rm test.txt
+     echo "usuwam"
+    ls
+ else
+
+   ls
+   echo "dodaje"
+   touch test.txt
+   ls 
+fi
+ read -p "podaj nazwe programu " app
+command=$(which $app) 
+echo $command
+if [ -f $command ]
+then 
+ echo "jest zainstalowany"
+else 
+ echo "nie jest zainstalowany"
+fi
+
